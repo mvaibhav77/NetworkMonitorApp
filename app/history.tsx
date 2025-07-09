5a9bc96e6ca8d2c8142dc82120e9c1b31f6c62af
Vaibhav Shukla <shukla.vaibhav1077@gmail.com>
2025-07-09 22:50:41 +0530

    fix: history page style fixed

--- app/history.tsx
+++ app/history.tsx
@@ -3,7 +3,7 @@ import { NetworkChange } from "@/types/NetworkChange";
 import { useTheme } from "@/utils/ThemeProvider";
 import { useNavigation } from "@react-navigation/native";
 import { format } from "date-fns";
-import { ArrowRight, Clock, MoveLeft } from "lucide-react-native";
+import { Clock, MoveLeft, MoveRight } from "lucide-react-native";
 import { useEffect, useState } from "react";
 import {
   FlatList,
@@ -32,7 +32,7 @@ function NetworkBadge({
 }) {
   return (
     <View
-      className={`px-2 py-0.5 rounded-full ${
+      className={`px-4 py-2 rounded-full ${
         variant === "outline"
           ? "border border-slate-400"
           : "bg-slate-800 dark:bg-slate-200"
@@ -65,24 +65,26 @@ export default function HistoryScreen() {
     return (
       <TouchableOpacity
         key={item.id}
-        className="p-4 rounded-lg bg-white/80 dark:bg-neutral-950 border border-slate-200 dark:border-slate-700 shadow-sm mb-2"
+        className="p-4 rounded-lg bg-white/80 dark:bg-neutral-950 border border-neutral-100 dark:border-neutral-500 mb-2 shadow-lg"
       >
         <View className="flex-row justify-between items-center">
-          <View className="flex-row items-center space-x-2">
-            <Clock size={16} color="#94a3b8" />
-            <View className="flex-row items-center space-x-2">
+          <View className="flex-row items-center gap-x-4">
+            <View className="flex-row items-center gap-x-2">
               <NetworkBadge type={item.from} variant="outline" />
-              <ArrowRight size={16} color="#94a3b8" />
+              <MoveRight size={16} color="#94a3b8" />
               <NetworkBadge type={item.to} />
             </View>
           </View>
-          <View className="items-end">
-            <Text className="text-sm font-medium text-slate-700 dark:text-slate-200">
-              {formatTime(dateObj)}
-            </Text>
-            <Text className="text-xs text-slate-500 dark:text-slate-400">
-              {formatDate(dateObj)}
-            </Text>
+          <View className="flex-row items-center gap-x-2">
+            <View className="items-end">
+              <Text className="text-sm ont-sans text-slate-600 dark:text-slate-300">
+                {formatTime(dateObj)}
+              </Text>
+              <Text className="text-xs ont-sans text-slate-600 dark:text-slate-300">
+                {formatDate(dateObj)}
+              </Text>
+            </View>
+            <Clock size={24} color="#94a3b8" />
           </View>
         </View>
       </TouchableOpacity>
@@ -91,7 +93,7 @@ export default function HistoryScreen() {
 
   return (
     <View className="flex-1 bg-slate-50 dark:bg-black pt-16 gap-6">
-      <View className="flex-row items-center justify-start gap-6 border-b border-slate-200 dark:border-slate-700 mb-2 px-6 pb-4">
+      <View className="flex-row items-center justify-start gap-6 border-b border-slate-200 dark:border-slate-700  px-6 pb-4">
         <TouchableOpacity onPress={() => navigation.goBack()}>
           <MoveLeft
             color={theme === "dark" ? "#ccc" : "#333"}
@@ -119,7 +121,11 @@ export default function HistoryScreen() {
           data={history}
           renderItem={renderItem}
           keyExtractor={(item) => item.id}
-          contentContainerStyle={{ paddingBottom: 40, paddingHorizontal: 16 }}
+          contentContainerStyle={{
+            paddingBottom: 40,
+            paddingHorizontal: 16,
+            gap: 8,
+          }}
           showsVerticalScrollIndicator={false}
         />
       )}
